b0fccb93b44a5d731cd22d6e5950bd42d3c14ad3
email <tamarkaluzny@gmail.com>
2022-07-05 23:00:51 +0300

    cleaner code

--- routes/locationsRoutes.js
+++ routes/locationsRoutes.js
@@ -3,14 +3,7 @@ const { get } = require("express/lib/response");
 router= express.Router();
 locationsRoutes = require("../controllers/locationsControllers");
 
-
-
-
-
 router.get("/", locationsRoutes.locationsControllers);
-
-
-
 
 module.exports = router;
 
