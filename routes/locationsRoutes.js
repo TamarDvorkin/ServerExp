27878ef3fc757422522ab909967f436d831e4a00
email <tamarkaluzny@gmail.com>
2022-07-06 16:28:40 +0300

    removed some variables and modules

--- routes/locationsRoutes.js
+++ routes/locationsRoutes.js
@@ -1,8 +1,6 @@
 const express =  require ("express");
-const { get } = require("express/lib/response");
 router= express.Router();
 locationsRoutes = require("../controllers/locationsControllers");
-
 router.get("/", locationsRoutes.locationsControllers);
 
 module.exports = router;
